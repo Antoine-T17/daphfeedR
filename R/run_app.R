5f1b411c5ed57b3e6f130479e041b80cc8f55cde
Antoine TOURRET <antoinetourretpro@gmail.com>
2026-02-26 17:09:09 +0100

    Commit 4
    from daphFeedR to daphfeedR

--- R/run_app.R
+++ R/run_app.R
@@ -1,8 +1,8 @@
 #' Run daphfeedR
 #' @export
 run_app <- function(..., launch.browser = TRUE) {
-  app_dir <- system.file("app", package = "daphFeedR")
-  if (app_dir == "") stop("App directory not found. Is 'daphFeedR' installed?")
+  app_dir <- system.file("app", package = "daphfeedR")
+  if (app_dir == "") stop("App directory not found. Is 'daphfeedR' installed?")
 
   withr::with_dir(app_dir, shiny::runApp(app_dir, launch.browser = launch.browser, ...))
 }
